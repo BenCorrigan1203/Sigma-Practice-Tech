271298f88095f8a5488a8bf0051f3c0dd0eeea6a
BenCorrigan1203 <bencorrigan99@gmail.com>
2023-03-24 16:43:54 +0000

    finishing scrabble tasks

--- interview.py
+++ interview.py
@@ -18,6 +18,7 @@ bag = {
 }
 
 def word_points(word_to_evaluate: str) -> int:
+    """Calculates the points for a word from the points for each letter"""
     word_to_evaluate =  word_to_evaluate.upper()
 
     point_total = 0
@@ -28,6 +29,7 @@ def word_points(word_to_evaluate: str) -> int:
 
 
 def create_rack(bag: dict, seed: int = 0) -> list:
+    """Create a game rack of 7 random letters, distributed via the game bag"""
     rack = []
     if seed != 0:
         random.seed(seed)
@@ -43,6 +45,7 @@ def create_rack(bag: dict, seed: int = 0) -> list:
 
 
 def is_valid_word(word: str, rack: list) -> bool:
+    """checks to see if a word can be made out of letters in a list"""
     test_rack = copy.deepcopy(rack)
     for letter in word:
         if letter.upper() not in test_rack:
@@ -52,6 +55,7 @@ def is_valid_word(word: str, rack: list) -> bool:
         
 
 def get_valid_words_in_rack(rack: list) -> dict:
+    """returns all the words that can be made out of letters in a game rack searching from dictionary.txt"""
     valid_words = {"Length 1": [], "Length 2": [], "Length 3": [], "Length 4": [], "Length 5": [], "Length 6": [], "Length 7": []}
     with open("dictionary.txt", mode="r", encoding="utf-8") as dictionary:
         for word in dictionary:
@@ -60,34 +64,60 @@ def get_valid_words_in_rack(rack: list) -> dict:
     return valid_words
 
 
-def find_longest_valid_word(rack: list) -> str:
+def find_longest_valid_word(rack: list) -> dict:
+    """Find the longest valid word in a game rack out of the valid words"""
     valid_words = get_valid_words_in_rack(rack)
     for i in range(7, 0, -1):
         if valid_words[f"Length {i}"]:
-            return valid_words[f"Length {i}"]
+            return {'word_length': i, 'words': valid_words[f"Length {i}"]}
         valid_words = get_valid_words_in_rack(rack)
 
 
-def find_highest_scoring_word(rack: list) -> dict:
+def highest_word_score_with_triple_letter(word_to_evaluate: str) -> dict:
+    """Calculates the best score a word can have depending on which letter is triple scored"""
+    word_to_evaluate =  word_to_evaluate.upper()
+    point_total = word_points(word_to_evaluate)
+    best_total_with_triple_letter = {'word_score': 0, "best_triple_letters": []}
+
+    for letter in word_to_evaluate:
+        points_with_triple_letter = point_total + (2 * letter_points[letter])
+        if points_with_triple_letter > best_total_with_triple_letter["word_score"]:
+            best_total_with_triple_letter["word_score"] = points_with_triple_letter
+            best_total_with_triple_letter["best_triple_letters"] = [letter]
+
+        elif points_with_triple_letter == best_total_with_triple_letter["word_score"]:
+            if letter not in best_total_with_triple_letter["best_triple_letters"]:
+                best_total_with_triple_letter["best_triple_letters"].append(letter)
+    return best_total_with_triple_letter
+
+
+def find_highest_scoring_word(rack: list, include_triple_letter: bool = True) -> dict:
+    """Returns a dict with the highest possible word score, along with the words, including triple letters if desired"""
     valid_words = get_valid_words_in_rack(rack)
-    highest_scorers = {}
+    highest_scorers = {"word_score": 0, "words": []}
     for word_length in valid_words.values():
         for word in word_length:
-            score = word_points(word)
-            if not highest_scorers:
-                highest_scorers[word] = score
-            elif score == list(highest_scorers.values())[0]:
-                highest_scorers[word] = score
-            elif score > list(highest_scorers.values())[0]:
-                highest_scorers = {word: score}
+            if include_triple_letter:
+                score_and_triple_letters = highest_word_score_with_triple_letter(word)
+                score = score_and_triple_letters['word_score']
+                if score > highest_scorers["word_score"]:
+                    highest_scorers["word_score"] = score
+                    highest_scorers['words'] = [{"word": word, "best_triple_letters": score_and_triple_letters['best_triple_letters']}]
+                elif score == highest_scorers["word_score"]:
+                    highest_scorers['words'].append({"word": word, "best_triple_letters": score_and_triple_letters['best_triple_letters']})
+            else:
+                score = word_points(word)
+                if score > highest_scorers["word_score"]:
+                    highest_scorers["word_score"] = score
+                    highest_scorers['words'] = [word]
+                elif score == highest_scorers["word_score"]:
+                    highest_scorers['words'].append(word)
     return highest_scorers
-            
-
 
 if __name__ == "__main__":
     rack = create_rack(bag)
     words = get_valid_words_in_rack(rack)
-
     print(find_longest_valid_word(rack))
-    print(find_highest_scoring_word(rack))
-    
+    print(find_highest_scoring_word(rack, False))
+    print(find_highest_scoring_word(rack, True))
+
